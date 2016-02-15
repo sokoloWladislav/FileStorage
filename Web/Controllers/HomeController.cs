78c30261e8ca7b8ed05b983c96caac5874ac0852
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2016-02-16 01:09:11 +0300

    add auth layout

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,28 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using BLL.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IUserService UserService
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<IUserService>();
+            }
+        }
+
+        private IFileService FileService
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<IFileService>();
+            }
+        }
+
         public ActionResult Index()
         {
+            ViewBag.Layout = User.Identity.IsAuthenticated ? "~/Views/Shared/AuthLayout.cshtml" : "~/Views/Shared/NotAuthLayout.cshtml";
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Layout = User.Identity.IsAuthenticated ? "~/Views/Shared/AuthLayout.cshtml" : "~/Views/Shared/NotAuthLayout.cshtml";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Public()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Layout = User.Identity.IsAuthenticated ? "~/Views/Shared/AuthLayout.cshtml" : "~/Views/Shared/NotAuthLayout.cshtml";
+            var fileConfig = new MapperConfiguration(cfg => { cfg.CreateMap<FileDTO, FileViewModel>(); });
+            IMapper fileMapper = fileConfig.CreateMapper();
+            List<FileViewModel> files = fileMapper.Map<IEnumerable<FileDTO>, List<FileViewModel>>(FileService.GetAllPublicFiles());
+            return View(files);
+        }
 
-            return View();
+        public ActionResult Popular()
+        {
+            ViewBag.Layout = User.Identity.IsAuthenticated ? "~/Views/Shared/AuthLayout.cshtml" : "~/Views/Shared/NotAuthLayout.cshtml";
+            var fileConfig = new MapperConfiguration(cfg => { cfg.CreateMap<FileDTO, FileViewModel>(); });
+            IMapper fileMapper = fileConfig.CreateMapper();
+            List<FileViewModel> files = fileMapper.Map<IEnumerable<FileDTO>, List<FileViewModel>>(FileService.GetAllPublicFiles());
+            //Should be fixed!
+            return View(files);
         }
     }
 }
